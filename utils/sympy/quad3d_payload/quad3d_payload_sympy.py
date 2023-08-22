953e209cc2b10e7768031455ba6f44426935bb46
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2023-08-22 15:47:54 +0200

    removed one line to check qc (for now)

--- utils/sympy/quad3d_payload/quad3d_payload_sympy.py
+++ utils/sympy/quad3d_payload/quad3d_payload_sympy.py
@@ -137,7 +137,6 @@ def writeC(f, step, Jx, Ju, Fx, Fu):
 
         Eigen::Ref<const Eigen::Vector3d> pos = x.head(3).head<3>();
         Eigen::Ref<const Eigen::Vector3d> qc  = x.segment(3, 3).head<3>();
-        CHECK_LEQ(std::abs((qc.norm() - 1.0)), 1e-6, AT);
         Eigen::Ref<const Eigen::Vector3d> vel = x.segment(6, 3).head<3>();
         Eigen::Ref<const Eigen::Vector3d> wc  = x.segment(9, 3).head<3>();
         Eigen::Vector4d q = x.segment(12, 4).head<4>().normalized();
