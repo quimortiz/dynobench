d68aa2fbcfdcccaec41243e6aa4f7740653decf0
Quim Ortiz - Work <quimortiz21@gmail.com>
2024-01-25 23:28:05 +0100

    fix format

--- src/quadrotor_payload_dynamics_autogen_n3_p.hpp
+++ src/quadrotor_payload_dynamics_autogen_n3_p.hpp
@@ -3,57 +3,29 @@
 // Auto generated file
 // Created at: 2023-09-06--10-43-33
 
-
 namespace dynobench {
 
-void calcV_n3_p(double* ff,
-            
-    double mp,
-    double arm_length,
-    double t2t,
-    const double *m,
-    const double *J_vx,
-    const double *J_vy,
-    const double *J_vz,
-    const double *l,
-            const double *x, const double *u);
+void calcV_n3_p(double *ff,
 
-void calcStep_n3_p(double* xnext, 
-    double mp,
-    double arm_length,
-    double t2t,
-    const double *m,
-    const double *J_vx,
-    const double *J_vy,
-    const double *J_vz,
-    const double *l, const double *x, const double *u, double dt);
+                double mp, double arm_length, double t2t, const double *m,
+                const double *J_vx, const double *J_vy, const double *J_vz,
+                const double *l, const double *x, const double *u);
 
-void calcJ_n3_p(
-        double* Jx, 
-        double* Ju, 
-        
-    double mp,
-    double arm_length,
-    double t2t,
-    const double *m,
-    const double *J_vx,
-    const double *J_vy,
-    const double *J_vz,
-    const double *l,
-         const double *x, const double *u);
+void calcStep_n3_p(double *xnext, double mp, double arm_length, double t2t,
+                   const double *m, const double *J_vx, const double *J_vy,
+                   const double *J_vz, const double *l, const double *x,
+                   const double *u, double dt);
 
-void calcF_n3_p(
-    double* Fx, 
-    double* Fu, 
-    
-    double mp,
-    double arm_length,
-    double t2t,
-    const double *m,
-    const double *J_vx,
-    const double *J_vy,
-    const double *J_vz,
-    const double *l,
-     const double *x, const double *u, double dt);
+void calcJ_n3_p(double *Jx, double *Ju,
 
-}
+                double mp, double arm_length, double t2t, const double *m,
+                const double *J_vx, const double *J_vy, const double *J_vz,
+                const double *l, const double *x, const double *u);
+
+void calcF_n3_p(double *Fx, double *Fu,
+
+                double mp, double arm_length, double t2t, const double *m,
+                const double *J_vx, const double *J_vy, const double *J_vz,
+                const double *l, const double *x, const double *u, double dt);
+
+} // namespace dynobench
--- src/quadrotor_payload_dynamics_autogen_n6_p.hpp
+++ src/quadrotor_payload_dynamics_autogen_n6_p.hpp
@@ -3,57 +3,29 @@
 // Auto generated file
 // Created at: 2023-09-06--10-44-23
 
-
 namespace dynobench {
 
-void calcV_n6_p(double* ff,
-            
-    double mp,
-    double arm_length,
-    double t2t,
-    const double *m,
-    const double *J_vx,
-    const double *J_vy,
-    const double *J_vz,
-    const double *l,
-            const double *x, const double *u);
+void calcV_n6_p(double *ff,
 
-void calcStep_n6_p(double* xnext, 
-    double mp,
-    double arm_length,
-    double t2t,
-    const double *m,
-    const double *J_vx,
-    const double *J_vy,
-    const double *J_vz,
-    const double *l, const double *x, const double *u, double dt);
+                double mp, double arm_length, double t2t, const double *m,
+                const double *J_vx, const double *J_vy, const double *J_vz,
+                const double *l, const double *x, const double *u);
 
-void calcJ_n6_p(
-        double* Jx, 
-        double* Ju, 
-        
-    double mp,
-    double arm_length,
-    double t2t,
-    const double *m,
-    const double *J_vx,
-    const double *J_vy,
-    const double *J_vz,
-    const double *l,
-         const double *x, const double *u);
+void calcStep_n6_p(double *xnext, double mp, double arm_length, double t2t,
+                   const double *m, const double *J_vx, const double *J_vy,
+                   const double *J_vz, const double *l, const double *x,
+                   const double *u, double dt);
 
-void calcF_n6_p(
-    double* Fx, 
-    double* Fu, 
-    
-    double mp,
-    double arm_length,
-    double t2t,
-    const double *m,
-    const double *J_vx,
-    const double *J_vy,
-    const double *J_vz,
-    const double *l,
-     const double *x, const double *u, double dt);
+void calcJ_n6_p(double *Jx, double *Ju,
 
-}
+                double mp, double arm_length, double t2t, const double *m,
+                const double *J_vx, const double *J_vy, const double *J_vz,
+                const double *l, const double *x, const double *u);
+
+void calcF_n6_p(double *Fx, double *Fu,
+
+                double mp, double arm_length, double t2t, const double *m,
+                const double *J_vx, const double *J_vy, const double *J_vz,
+                const double *l, const double *x, const double *u, double dt);
+
+} // namespace dynobench
