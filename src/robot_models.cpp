3c25b19fc8e14b2eea51cc4ee4061978fb418db8
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2025-06-27 10:35:33 +0200

    add mujoco model

--- src/robot_models.cpp
+++ src/robot_models.cpp
@@ -43,7 +43,7 @@
 // #define dynobench_models "../dynoplan/dynobench/models/"
 #include "dynobench/integrator1_2d.hpp"
 #include "dynobench/integrator2_3d.hpp"
-
+#include "dynobench/mujoco_quadrotors_payload.hpp"
 namespace dynobench {
 
 std::unique_ptr<Model_robot> robot_factory(const char *file,
@@ -94,6 +94,8 @@ std::unique_ptr<Model_robot> robot_factory(const char *file,
   return std::make_unique<DintegratorCables>(file, p_lb, p_ub);
   }  else if (dynamics == "unicyclesWithRods") {
   return std::make_unique<unicyclesWithRods>(file, p_lb, p_ub);
+  } else if (dynamics == "mujocoquadspayload") {
+  return std::make_unique<Model_MujocoQuadsPayload>(file, p_lb, p_ub);
   } else {
     ERROR_WITH_INFO("dynamics not implemented: " + dynamics);
   }
