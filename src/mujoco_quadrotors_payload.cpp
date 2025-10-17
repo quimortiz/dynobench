3c97df9d44d6741ab2753b81e7ce2d2ca34c6ad8
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2025-10-17 17:25:53 +0200

    update envs (remove unused ones)

--- src/mujoco_quadrotors_payload.cpp
+++ src/mujoco_quadrotors_payload.cpp
@@ -164,10 +164,10 @@ Model_MujocoQuadsPayload::Model_MujocoQuadsPayload(
   }
 
   u_weight.resize(4 * params.num_robots);
-  u_weight.setConstant(.7);
+  u_weight.setConstant(.5);
 
   x_weightb = Vxd::Zero(nx);
-  x_weightb.tail(m->nq+m->nv) = 350*Vxd::Ones(nx);
+  x_weightb.tail(m->nq+m->nv) = 400*Vxd::Ones(nx);
   // x_weightb.tail(m->nv) = 300*Vxd::Ones(nx);
   x_weightb.segment(3 ,4) = Eigen::VectorXd::Zero(4); // paylaod quat 
   x_weightb.segment(7*(params.num_robots+1) + 3 ,3) = Eigen::VectorXd::Zero(3); // ang vel payload
